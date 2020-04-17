e9ea69f714906be6bff174c6e0e8cdec79fa8250
Vinicius Castro <costacastrovinicius7@gmail.com>
2020-04-16 23:54:08 -0300

    feature(app): React sample app improved

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,25 @@
 import React, {useEffect, useState} from 'react';
-import {Button} from '@material-ui/core';
 import Provider from './provider';
 import './App.css';
+import {Card, CardHeader} from '@material-ui/core';
 
 function App() {
-  const [clicks, setClicks] = useState(0);
   const [courses, setCourses] = useState([]);
+
+  const getCourseImage = async url => {
+    const response = await fetch(url + '?token=' + document.wstoken);
+    const blob = await response.blob();
+    return URL.createObjectURL(blob);
+  };
 
   const getRecentlyAccessedCourses = async () => {
     const {userid} = await Provider.callMoodleWebService('core_webservice_get_site_info');
     const courses = await Provider.callMoodleWebService('core_enrol_get_users_courses', {userid});
+
+    for (const course of courses) {
+      course.image = await getCourseImage(course.overviewfiles[0]?.fileurl);
+    }
+
     courses.sort((a, b) => {
       if (a.lastaccess > b.lastaccess) {
         return -1;
@@ -28,9 +38,18 @@ function App() {
 
   return (
     <div className="App">
-      {courses.map((course) => (
-        <p>{course.displayname}</p>
-      ))}
+      <ul style={{listStyleType: 'none'}}>
+        {courses.map((course) => (
+          <li>
+            <div style={{float: "left", paddingRight: 15}}>
+              <Card>
+                <CardHeader title={course.displayname} />
+                <img src={course.image} height="200" width="345" style={{objectFit: 'cover'}} />
+              </Card>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
